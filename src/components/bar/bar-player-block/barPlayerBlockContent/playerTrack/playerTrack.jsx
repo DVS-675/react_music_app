a51f7d6e0a5c6edf485cb226c4ca19192537f371
DVS-675 <dmitryvs20170@gmail.com>
2023-08-23 20:59:37 +0500

    add like, dislike buttons in bar

--- src/components/bar/bar-player-block/barPlayerBlockContent/playerTrack/playerTrack.jsx
+++ src/components/bar/bar-player-block/barPlayerBlockContent/playerTrack/playerTrack.jsx
@@ -3,10 +3,47 @@ import { ReactComponent as Like } from "../../../../../img/icon/like.svg"
 import { ReactComponent as Dislike } from "../../../../../img/icon/dislike.svg"
 import { Skeleton } from "../../../../skeleton/skeleton.jsx"
 import classes from "./playerTrack.module.css"
-import { useSelector } from "react-redux"
-
+import { useDispatch, useSelector } from "react-redux"
+import {
+  useDislikeTrackMutation,
+  useLikeTrackMutation,
+} from "../../../../../services/tracks"
+import { useEffect, useState } from "react"
+import { useTokenContext } from "../../../../../contexts/token"
+import { setLikesState } from "../../../../../store/actions/creators/tracks"
 
 export const PlayerContentTrack = ({ playTrack, loading }) => {
+  const { id } = playTrack
+  const [likeButtonState, setLikeButtonState] = useState()
+  const likesState = useSelector((store) => store.tracks.likesState)
+  const [dislikeTrigger] = useDislikeTrackMutation()
+  const [likeTrigger] = useLikeTrackMutation()
+  const dispatch = useDispatch()
+  const { token } = useTokenContext()
+
+  const toggleDislike = () => {
+    setLikeButtonState(false)
+    const newLikesState = { ...likesState }
+    const { access } = token
+    const args = { id, token: access }
+    newLikesState[id] = false
+    dispatch(setLikesState(newLikesState))
+    dislikeTrigger(args)
+  }
+
+  const toggleLike = () => {
+    setLikeButtonState(true)
+    const newLikesState = { ...likesState }
+    const { access } = token
+    const args = { id, token: access }
+    newLikesState[id] = true
+    dispatch(setLikesState(newLikesState))
+    likeTrigger(args)
+  }
+
+  useEffect(() => {
+    setLikeButtonState(likesState[id])
+  }, [id, likesState, likeButtonState])
   return (
     <div className={classes.player}>
       <div className={classes.track}>
@@ -37,11 +74,31 @@ export const PlayerContentTrack = ({ playTrack, loading }) => {
         )}
       </div>
       <div className={classes.like_dis}>
-        <div className={`${classes.like} ${classes._btn_icon}`}>
-          <Like className={classes.like_svg} alt="like" />
+        <div
+          onClick={toggleLike}
+          className={`${classes.like} ${classes._btn_icon}`}
+        >
+          <Like
+            // fill={likeButtonState ? "#ad61ff" : "transparent"}
+            // stroke={likeButtonState ? "#ad61ff" : "#696969"}
+            className={
+              likeButtonState ? classes.like_svg_active : classes.like_svg
+            }
+            alt="like"
+          />
         </div>
-        <div className={`${classes.dislike} ${classes._btn_icon}`}>
-          <Dislike className={classes.dislike_svg} alt="dislike" />
+        <div
+          onClick={toggleDislike}
+          className={`${classes.dislike} ${classes._btn_icon}`}
+        >
+          <Dislike
+            // fill={likeButtonState ? "transparent" : "#ad61ff"}
+            // stroke={likeButtonState ? "#696969" : "#ad61ff"}
+            className={
+              likeButtonState ? classes.dislike_svg : classes.dislike_svg_active
+            }
+            alt="dislike"
+          />
         </div>
       </div>
     </div>
